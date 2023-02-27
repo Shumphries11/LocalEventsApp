7213926fee808dd77530be29502daac5ee0c18e4
Santita Humphries <santitahumphries@gmail.com>
2023-02-27 14:20:10 -0600

    Setup View Controllers and Storyboards

---
+++ Events/Events/View Controllers/EventsViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class EventsViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ Events/Events/View Controllers/TicketViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class TicketViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
